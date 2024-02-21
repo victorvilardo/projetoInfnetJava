a3c0ca71850ac3e6583b192599cf2f4038e2deb8
victorvilardo <victorvilardo@hotmail.com>
2024-02-21 20:47:21 -0300

    Initial commit

---
+++ src/main/java/infnet/com/br/projetoInfnetJava/ProjetoInfnetJavaApplication.java
@@ -0,0 +1,13 @@
+package infnet.com.br.projetoInfnetJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoInfnetJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoInfnetJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/infnet/com/br/projetoInfnetJava/ProjetoInfnetJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package infnet.com.br.projetoInfnetJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoInfnetJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
