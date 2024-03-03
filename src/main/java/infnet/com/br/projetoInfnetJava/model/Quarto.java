f9911b5a6b66a161e44be1b3bcded83b1466abd0
Denise FC <deniseuff@gmail.com>
2024-03-03 13:25:11 -0300

    adicionando a herança dos quartos

--- src/main/java/infnet/com/br/projetoInfnetJava/model/Quarto.java
+++ src/main/java/infnet/com/br/projetoInfnetJava/model/Quarto.java
@@ -1,21 +1,22 @@
 package infnet.com.br.projetoInfnetJava.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 
 @MappedSuperclass
-@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"id"})
-public class Quarto implements Serializable {
+public abstract class Quarto implements Serializable {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "O nome não pode estar vazio")
     private String nome;
 }
--- src/main/java/infnet/com/br/projetoInfnetJava/model/QuartoCasal.java
+++ src/main/java/infnet/com/br/projetoInfnetJava/model/QuartoCasal.java
@@ -1,21 +1,15 @@
 package infnet.com.br.projetoInfnetJava.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import jakarta.persistence.*;
+import lombok.*;
+import java.io.Serial;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class QuartoCasal{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
+@EqualsAndHashCode(callSuper = false)
+public class QuartoCasal extends Quarto{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
 }
